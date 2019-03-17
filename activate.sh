af7c953b3e62a77dbb9e1928921f7e6a40d0efe3
Makoto Nozawa <corin.0724@gmail.com>
2019-03-17 15:19:36 +0900

    modify activate.sh

--- activate.sh
+++ activate.sh
@@ -1,4 +1,4 @@
-+#!/bin/sh
+#!/bin/sh
 
 SCRIPT_PATH=$(dirname ${0})
 RET=$(ps alx | grep mhw_bot | grep -v grep | grep -v activate | wc -l)
@@ -7,7 +7,7 @@ echo ${RET}
 if [ ${RET} -eq 0 ]; then
     echo "activate bot"
     cd "${SCRIPT_PATH}"
-    python3 ./mhw_bot.py > /dev/null 2>&1 &
+    /usr/bin/python3 ./mhw_bot.py > /dev/null 2>&1 &
 else
     echo "running"
 fi
